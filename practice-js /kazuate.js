28de914a21107eb3f08908e2b2ef2dcd88557c9e
G284382022 <228438st.takushoku-u.ac.jp>
2023-05-25 16:01:25 +0900

    kai

--- practice-js /kazuate.js
+++ practice-js /kazuate.js
@@ -9,7 +9,15 @@ let kaisu = 0;
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 
-hantei();
+let b1=document.querySelector('#kaito');
+b1.addEventListener('click',greeting)
+
+function greeting() {
+	let i = document.querySelector('input[name="seisu"]');
+	let seisu = i.value;
+	console.log('こんにちは! ' + seisu + ' さん');
+}
+
 
 let kk=document.querySelector('span#kaisu');
 let aa=document.querySelector('span#answer');
